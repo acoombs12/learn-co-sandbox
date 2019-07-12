7a548d9fa599e572d6895457fdaa314103b87eb2
allison Coombs <allison@allisoncoombs.com>
2019-07-12 03:50:10 +0000

    Automatically backed up by Learn

---
+++ defining-methods.rb
@@ -0,0 +1,19 @@
+#def say_hello_world_five_times
+ # message = "Hello World!"
+  #puts message
+  #puts message
+  #puts message
+  #puts message
+  #puts message
+#end
+#say_hello_world_five_times
+##other work
+#say_hello_world_five_times
+def greeting
+  puts "Hello World!"
+end
+greeting # executing the method by name
+#=> "Hello World"
+greeting # executing the method again
+#=> "Hello World"
+
---
+++ repetition.rb
@@ -0,0 +1,11 @@
+#count = 0
+#while count < 3 do # A boolean expression using the bit of data
+#puts "I am the #{count}, I love to count!" # Work
+#count += 1 # take the value of count, ad one to it and then reassign that result to count
+#end
+magic_exit_number = 7
+count = 0
+while count < 10 && count != magic_exit_number do
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
+end
